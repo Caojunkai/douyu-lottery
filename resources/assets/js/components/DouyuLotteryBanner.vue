b65d76c15beea5a02cbcb3e3da38796509d94036
Fitz <caojunkaiv@gmail.com>
2017-05-05 10:06:20 +0800

    change view to vue and element ui

---
+++ resources/assets/js/components/DouyuLotteryBanner.vue
@@ -0,0 +1,25 @@
+<template>
+    <el-row>
+        <el-col :span="24">
+            <div class="grid-content bg-purple-dark">
+                <img src="../../images/douyu_lotter_banner.png" alt="">
+            </div>
+        </el-col>
+    </el-row>
+</template>
+
+<style scoped>
+    .el-row{
+        margin-top: -22px;
+    }
+    .grid-content img{
+        width:100%;
+        height:auto;
+    }
+</style>
+
+<script>
+  export default{
+    name: 'DouyuLotteryBanner'
+  }
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">面板</div>
-                    <div class="panel-body">
-                        23333333
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <example :my-message="2333"></example>
+    <douyu-lottery-banner></douyu-lottery-banner>
+    11111
 @endsection

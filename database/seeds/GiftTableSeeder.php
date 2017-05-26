a7701b9bc2585561772b47abd6ce82a1f89a537a
Fitz <caojunkaiv@gmail.com>
2017-05-26 20:38:10 +0800

    add 5W Seeder

--- database/seeds/GiftTableSeeder.php
+++ database/seeds/GiftTableSeeder.php
@@ -9,12 +9,11 @@ class GiftTableSeeder extends Seeder
      */
     public function run()
     {
-        $str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $d = ['计算机编程类','生活百科','是的','我们正','是这么计','划的','通过','与客户的接','开发FamSpam','甚至是','开发','GitHub','本身'];
+        $d = ['计算机编程类','这是最好的时代','这是最坏的时代','这是智慧的时代','这是愚蠢的时代','这是信仰的时期','这是怀疑的时期','这是光明的季节','这是黑暗的季节','这是希望之春','这是失望之冬','GitHub','们面前有着各样事物','人们面前一无所有','人们正在直登天堂','人们正在直下地狱'];
         $data = [];
-        for ($i=0; $i < 10000; $i++) {
+        for ($i=0; $i < 50000; $i++) {
             $tmp = [];
-            $tmp['douyu_name'] = $d[random_int(0,12)];
+            $tmp['douyu_name'] = $d[random_int(0,count($d)-1)];
             $tmp['douyu_id'] = str_random(8);
             $tmp['vote_id'] = random_int(1,4);
             $tmp['vote_time'] = date('Y-m-d H:i:s',time());
